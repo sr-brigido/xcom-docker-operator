dd471f18646a7ab043ce97419e5fc7a695c81814
Gabriel Ronchi Brigido <gabrielbrigido1@gmail.com>
2024-05-18 19:48:26 -0300

    feat: adicionando tempo para a task1

--- src/task1.py
+++ src/task1.py
@@ -1,11 +1,16 @@
 from datetime import datetime
 import pickle
+import time
+
+time.sleep(20)
 
 dataHoje = datetime.today().strftime('%d/%m/%Y')
 
-saida = {
-    'data': dataHoje
-}
+print(dataHoje)
 
-with open('/tmp/script.out', 'wb+') as output:
-    pickle.dump(saida,output)
+# saida = {
+#     'data': dataHoje
+# }
+
+# with open('/tmp/script.out', 'wb+') as output:
+#     pickle.dump(saida,output)
