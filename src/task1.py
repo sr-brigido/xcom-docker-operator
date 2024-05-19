e9e453d2bbf1a09b0d0d61a9e2cdc8121599be17
Gabriel Ronchi Brigido <gabrielbrigido1@gmail.com>
2024-05-19 01:59:28 -0300

    fix: ajustando caminhos do taskipy

--- src/task1.py
+++ src/task1.py
@@ -6,11 +6,9 @@ time.sleep(20)
 
 dataHoje = datetime.today().strftime('%d/%m/%Y')
 
-print(dataHoje)
+saida = {
+    'data': dataHoje
+}
 
-# saida = {
-#     'data': dataHoje
-# }
-
-# with open('/tmp/script.out', 'wb+') as output:
-#     pickle.dump(saida,output)
+with open('/tmp/script.out', 'wb+') as output:
+    pickle.dump(saida,output)
